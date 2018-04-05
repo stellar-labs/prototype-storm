623e4368e99fbfb6dc50c513b1f196e666ac1e1c
Amin NAIRI <nairi.amin@gmail.com>
2018-04-05 22:29:33 +0200

    :tada: initial commit

---
+++ spec/shuffle.spec.js
@@ -0,0 +1,7 @@
+'use strict'
+import shuffle from '../src/js/shuffle.js'
+describe('shuffle String prototype', () => {
+  it('should return a string different than what it was previously', () => {
+    expect('Hello world!'.shuffle()).not.toBe('Hello world!')
+  })
+})
