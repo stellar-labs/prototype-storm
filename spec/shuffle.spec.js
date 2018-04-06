9477326ed9a9278ebf067179e68e62141d2f338b
khalyomede <khalyomede@gmail.com>
2018-04-06 23:19:54 +0200

    moved jasmine test files to mocha test folder

--- spec/shuffle.spec.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-import shuffle from '../src/js/shuffle.js'
-describe('shuffle String prototype', () => {
-  it('should return a string different than what it was previously', () => {
-    expect('Hello world!'.shuffle()).not.toBe('Hello world!')
-  })
-})
--- spec/stripTags.spec.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-import stripTags from '../src/js/stripTags.js'
-describe('stripTags String prototype', () => {
-  it('should return the string without html tags', () => {
-    expect('<li><a href="/home.html">Home Page</a></li>'.stripTags()).toBe('Home Page')
-  })
-})
