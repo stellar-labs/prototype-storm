7769b950b7ebdec9a0684a48c5fc0938c392111e
khalyomede <khalyomede@gmail.com>
2018-04-07 13:23:35 +0200

    added missing comma

--- test/string/ellipsis.spec.js
+++ test/string/ellipsis.spec.js
@@ -1,16 +1,21 @@
-'use strict'
-import ellipsis from '../src/js/ellipsis.js'
+'use strict';
+
+import ellipsis from '../../src/js/ellipsis.js';
+
 describe('ellipsis String prototype', () => {
   it('should return the string ellipsized', () => {
     expect('Hello world!'.ellipsis({max: 5})).toBe('Hello...')
-  })
+  });
+
   it('should return the string not ellipsized', () => {
     expect('Hello world!'.ellipsis({max: 12})).toBe('Hello world!')
-  })
+  });
+
   it('should return the string ellipsized with a custom ellipsis delimitor', () => {
     expect('Hello world!'.ellipsis({max: 5, end: '[...]'})).toBe('Hello[...]')
-  })
+  });
+
   it('should return the ellipsis only', () => {
     expect('Hello world!'.ellipsis({max: -5})).toBe('...')
-  })
-})
+  });
+});
