faf83bab18d0030b3b8c7454d8e9aa387e423ebf
khalyomede <khalyomede@gmail.com>
2018-04-07 13:20:41 +0200

    moved tests to the folder they belong to

---
+++ test/string/nl2br.spec.js
@@ -0,0 +1,22 @@
+'use strict'
+import nl2br from '../src/js/nl2br.js'
+describe('nl2br String prototype', () => {
+  it('should return a string with break line from a string containing new lines', () => {
+    expect('Hello\nWorld!'.nl2br()).toBe('Hello<br>World!')
+  })
+  it('should return a string with strict break line from a string containing new lines when the argument strict is set to true', () => {
+    expect('Hello\nWorld!'.nl2br({strict: true})).toBe('Hello<br/>World!')
+  })
+  it('should return a string with non-strict break line from a string containing new lines when the argument strict is set to false', () => {
+    expect('Hello\nWorld!'.nl2br({strict: false})).toBe('Hello<br>World!')
+  })
+  it('should return the same string when there is no line return', () => {
+    expect('Hello World!'.nl2br()).toBe('Hello World!')
+  })
+  it('should return the same string when there is no line return and the strict argument is set to false', () => {
+    expect('Hello World!'.nl2br({strict: false})).toBe('Hello World!')
+  })
+  it('should return the same string when there is no line return and the strict argument is set to true', () => {
+    expect('Hello World!'.nl2br({strict: true})).toBe('Hello World!')
+  })
+})
---
+++ test/string/stripTags.spec.js
@@ -0,0 +1,7 @@
+'use strict'
+import stripTags from '../src/js/stripTags.js'
+describe('stripTags String prototype', () => {
+  it('should return the string without html tags', () => {
+    expect('<li><a href="/home.html">Home Page</a></li>'.stripTags()).toBe('Home Page')
+  })
+})
