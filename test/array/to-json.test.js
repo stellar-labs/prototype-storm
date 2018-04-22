ea96c03b6b86ebc03f0c25d7a0222e835f18ca8c
khalyomede <khalyomede@gmail.com>
2018-04-22 11:21:11 +0200

    added Array.toJson tests

---
+++ test/array/to-json.test.js
@@ -0,0 +1,30 @@
+'use strict'
+
+var should = require('chai').should();
+
+import toJson from '../../src/js/array/to-json.js';
+
+describe('Array', function() {
+	describe('toJson', function() {
+		it('should works for empty array', function() {
+			[].toJson().should.equal('[]');
+		});
+
+		it('should work for integer array', function() {
+			[1, 2, 3].toJson().should.equal('[1,2,3]');
+		});
+
+		it('should work for object array', function() {
+			var array = [
+				{
+					name: 'John'
+				},
+				{
+					name: 'Foo'
+				}
+			];
+
+			array.toJson().should.equal('[{"name":"John"},{"name":"Foo"}]');
+		});
+	});
+});
