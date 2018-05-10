e6d16cbf2315bd638c0d9fcb596e0a932adc007c
khalyomede <khalyomede@gmail.com>
2018-05-10 17:31:15 +0200

    added jsdoc

--- src/js/empty.js
+++ src/js/empty.js
@@ -1,6 +1,18 @@
 'use strict';
 export default (() => {
   if ('empty' in Object === false) {
+    /**
+     * @summary Returns true if the element is empty, else returns false.
+     * @return {Boolean}
+     * @example {}.empty() // true
+     * @example [].empty() // true
+     * @example ''.empty() // true
+     * @example 0.empty() // true
+     * @since 0.1.0
+     * @see https://github.com/stellar-labs/prototype-storm/v0.1.0/dist/empty.min.js
+     * @see https://github.com/stellar-labs/prototype-storm/empty.js
+     * @see https://github.com/stellar-labs/prototype-storm/test/empty.js
+     */
     Object.prototype.empty = function() {
       if (this.constructor === Object) {
         return Object.keys(this).length === 0;
