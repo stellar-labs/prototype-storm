001a6af5217af4aaf7103aa1a5a28129442a9544
Amin NAIRI <nairi.amin@gmail.com>
2018-05-10 18:01:45 +0200

    :bug: fixed bug where the sum was always zero on non-empty arrays

--- src/js/sum.js
+++ src/js/sum.js
@@ -3,7 +3,7 @@ export default (() => {
   if ('sum' in Object === false) {
     Object.prototype.sum = function() {
       if (this.constructor === Array) {
-        this.reduce((accumulator, current) => {
+        return this.reduce((accumulator, current) => {
           return accumulator + Number(current)|0;
         }, 0);
       }
