56ceda326498a0dd69b81bc0abe1fb7aa2cc738f
Amin NAIRI <nairi.amin@gmail.com>
2018-05-10 17:34:57 +0200

    :write_check_mark: added test for average with several scalar objects

--- src/test/test.spec.js
+++ src/test/test.spec.js
@@ -5,28 +5,24 @@ import '../js/map.js';
 import '../js/average.js';
 import '../js/empty.js';
 describe('Prototype Storm', () => {
-  describe('Flatten', () => {
-    it('Should return a flattened array', () => {
-      expect([1, [2, [3]]].flatten()).to.deep.equal([1, 2, 3]);
-    });
-    it('Should return the same array', () => {
-      expect([1, 2, 3].flatten()).to.deep.equal([1, 2, 3]);
-    });
-    it('Should return an empty array', () => {
-      expect([].flatten()).to.deep.equal([]);
-    });
-  });
-  describe('Map', () => {
-    it('Should return the values of the object', () => {
-      expect({a: 1, b: 2, c: 3}.map(value => value)).to.deep.equal([1, 2, 3]);
-    });
-    it('Should return the indexes of the object', () => {
-      expect({a: 1, b: 2, c: 3}.map((value, index) => index)).to.deep.equal(['a', 'b', 'c']);
-    });
-  });
   describe('Average', () => {
     it('Should return the average of an array', () => {
-      expect([1, 2, 3].average()).to.equal(6);
+      expect([1, 2, 3].average()).to.equal(2);
+    });
+    it('Should return zero with an empty array', () => {
+      expect([].average()).to.equal(0);
+    });
+    it('Should return zero with an array of letters\' string', () => {
+      expect(['esmodule', 'commonjs', 'AMD'].average()).to.equal(0);
+    });
+    it('Should return zero with an object', () => {
+      expect({a: 1, b: 2, c: 3}.average()).to.equal(0);
+    });
+    it('Should return zero with a string', () => {
+      expect({a: 1, b: 2, c: 3}.average()).to.equal(0);
+    });
+    it('Should return the number with a number', () => {
+      expect((5).average()).to.equal(5);
     });
   });
   describe('Empty', () => {
@@ -53,6 +49,34 @@ describe('Prototype Storm', () => {
     });
     it('Should return false on non-zero number', () => {
       expect((123).empty()).to.deep.equal(false);
+    });
+  });
+  describe('Flatten', () => {
+    it('Should return a flattened array', () => {
+      expect([1, [2, [3]]].flatten()).to.deep.equal([1, 2, 3]);
+    });
+    it('Should return the same array', () => {
+      expect([1, 2, 3].flatten()).to.deep.equal([1, 2, 3]);
+    });
+    it('Should return an empty array', () => {
+      expect([].flatten()).to.deep.equal([]);
+    });
+    it('Should return a flattened object', () => {
+      expect({a: 1, b: {c: 2, d: {e: 3}}}.flatten()).to.deep.equal({a: 1, c: 2, e: 3});
+    });
+    it('Should return the same object', () => {
+      expect({a: 1, b: 2, c: 3}.flatten()).to.deep.equal({a: 1, b: 2, c: 3});
+    });
+    it('Should return an empty object', () => {
+      expect({}.flatten()).to.deep.equal({});
+    });
+  });
+  describe('Map', () => {
+    it('Should return the values of the object', () => {
+      expect({a: 1, b: 2, c: 3}.map(value => value)).to.deep.equal([1, 2, 3]);
+    });
+    it('Should return the indexes of the object', () => {
+      expect({a: 1, b: 2, c: 3}.map((value, index) => index)).to.deep.equal(['a', 'b', 'c']);
     });
   });
 });
