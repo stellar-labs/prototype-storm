2604e703178a2914ff5c5d28ce1ffb817af472ab
khalyomede <>
2018-05-11 22:57:54 +0200

    added comments

--- src/js/sum.js
+++ src/js/sum.js
@@ -1,6 +1,15 @@
 'use strict';
 export default (() => {
   if ('sum' in Object === false) {
+    /**
+     * @summary Returns the sum of the elements.
+     * @returns {Number}
+     * @example [1, 2, 3].sum() // 6
+     * @since 0.1.0
+     * @see https://cdn.rawgit.com/stellar-labs/prototype-storm/v0.1.0/dist/sum.js
+     * @see https://github.com/stellar-labs/prototype-storm/sum.js
+     * @see https://github.com/stellar-labs/prototype-storm/test/sum.js
+     */
     Object.prototype.sum = function() {
       if (this.constructor === Array) {
         return this.reduce((accumulator, current) => {
