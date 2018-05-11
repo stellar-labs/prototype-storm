c9ec9a23a198ac715612fb9766f16c3faed32d30
khalyomede <>
2018-05-11 22:52:43 +0200

    added comments

--- src/js/nl2br.js
+++ src/js/nl2br.js
@@ -1,6 +1,16 @@
 'use strict';
 export default (() => {
   if ('nl2br' in Object === false) {
+    /**
+     * @summary Turns any line return into HTML line return <br />
+     * @param {Boolean} options.strict=false when set to true, it will output strict HTML <br /> tag
+     * @returns {*}
+     * @example "hello\r\nworld".nl2br() // "hello<br />world"
+     * @since 0.1.0
+     * @see https://cdn.rawgit.com/stellar-labs/prototype-storm/v0.1.0/dist/nl2br.js
+     * @see https://github.com/stellar-labs/prototype-storm/nl2br.js
+     * @see https://github.com/stellar-labs/prototype-storm/test/nl2br.js
+     */
     Object.prototype.nl2br = function({strict = false} = {}) {
       if (this.constructor === String) {
         return this.replace(/\n|\r\n/gm, strict ? '<br/>' : '<br>');
