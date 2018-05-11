415af1b2f6a88675a7aeaaedfd1c1c671cf5d7b9
khalyomede <>
2018-05-11 22:46:52 +0200

    added comments

--- src/js/last.js
+++ src/js/last.js
@@ -4,6 +4,13 @@ export default (() => {
     /**
      * @todo do some testing for last method
      * @todo extend last to other scalar types
+     * @summary Returns the last element of the variable
+     * @returns {*}
+     * @example [1, 2, 3].last() // 3
+     * @since 0.1.0
+     * @see https://cdn.rawgit.com/stellar-labs/prototype-storm/v0.1.0/dist/last.js
+     * @see https://github.com/stellar-labs/prototype-storm/last.js
+     * @see https://github.com/stellar-labs/prototype-storm/test/last.js
      */
     Object.prototype.last = function() {
       if (this.constructor === Array) {
