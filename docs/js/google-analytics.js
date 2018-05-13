76cfe73a1e76b44cf8f0923ad7bf7e67cf845cf3
khalyomede <>
2018-05-13 10:24:02 +0200

    added google analytics

---
+++ docs/js/google-analytics.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded', function() {
+    window.dataLayer = window.dataLayer || [];
+  
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    
+    gtag('js', new Date());
+
+    gtag('config', 'UA-119139201-1');
+});
