03d740e0e9e3099f898caac0a139a4e10dfe52c4
khalyomede <>
2018-05-15 22:58:17 +0200

    fixed unnecessary variable && statement

--- src/js/max.js
+++ src/js/max.js
@@ -1,57 +1,57 @@
 'use strict';
 
 if( 'max' in Object.prototype === false ) {
-    Object.prototype.max = function() {
-        // Array
-        if( this.constructor === Array ) {
-            if( this.length === 0 ) {
-                return null;    
-            }
+  Object.prototype.max = function() {
+    // Array
+    if( this.constructor === Array ) {
+      if( this.length === 0 ) {
+        return null;    
+      }
 
-            let max = this[0];
+      let max = this[0];
 
-            for( let i = 1; i < this.length; i++ ) {
-                if( this[i] > max ) {
-                    max = this[i];
-                }
-            }
-
-            return max !== null ? Number(max) : max;
+      for( let i = 1; i < this.length; i++ ) {
+        if( this[i] > max ) {
+          max = this[i];
         }
+      }
 
-        // Object
-        if( this.constructor === Object ) {
-            const length = Object.keys(this).length;
+      return max !== null ? Number(max) : max;
+    }
 
-            let result = null;
+    // Object
+    if( this.constructor === Object ) {
+      let result = null;
 
-            for( let key in this ) {
-                let value = this[key];
+      for( let key in this ) {
+        let value = this[key];
                   
-                if( isNaN(value) === false && value > result ) {
-                    result = value;
-                }
-                else {
-                    let is_object = false;
-
-                    try {
-                        is_object = 'constructor' in value && value.constructor === Object;
-                    }
-                    catch(error) {}
-
-                    if( is_object === true ) {
-                        let max = value.max();
-
-                        if( max > result ) {
-                            result = max;
-                        }
-                    }
-                }
-            }
-
-            return result !== null ? Number(result) : result;
+        if( isNaN(value) === false && value > result ) {
+          result = value;
         }
-    };
+        else {
+          let is_object = false;
+
+          try {
+            is_object = 'constructor' in value && value.constructor === Object;
+          }
+          catch(error) {
+            is_object = false;
+          }
+
+          if( is_object === true ) {
+            let max = value.max();
+
+            if( max > result ) {
+              result = max;
+            }
+          }
+        }
+      }
+
+      return result !== null ? Number(result) : result;
+    }
+  };
 }
 
 export default Object.prototype.max;
