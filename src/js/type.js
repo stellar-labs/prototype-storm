aa3b83bd7a1a61e44f732256d2e2c80f6cd9e1a2
khalyomede <>
2018-05-19 20:31:31 +0200

    added prototype type

--- src/js/type.js
+++ src/js/type.js
@@ -1,15 +1,19 @@
 'use strict';
-export default (() => {
-  if ('type' in Object === false) {
+
+if( 'type' in Object.prototype === false ) {
     /**
-     * @todo do some testing for the type method
-     * @todo maybe explode this method in <TYPE>.prototype.type() & <TYPE>.prototype.is(type) ?
-     */
-    Object.prototype.type = function(comparison = undefined) {
-      if (comparison) {
-        return this.constructor === comparison;
-      }
-      return this.constructor;
+     * @summary Returns the type of the element.
+     * @returns {*}
+     * @example [1, 2].type() // Array
+     * @example [1, 2].type({ toString: true }) // "array"
+     * @since 0.1.0
+     * @see https://cdn.rawgit.com/stellar-labs/prototype-storm/master/dist/type.min.js
+     * @see https://github.com/stellar-labs/prototype-storm/blob/master/src/js/type.js
+     * @see https://github.com/stellar-labs/prototype-storm/blob/master/test/type.js
+     */ 
+    Object.prototype.type = function(options = { toString: false }) {
+        return options.toString === true ? this.constructor.name.toLocaleLowerCase() : this.constructor;
     };
-  }
-})();
+}
+
+export default Object.prototype.type;
