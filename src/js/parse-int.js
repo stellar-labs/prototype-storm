8f3fccf25dabcb604dec46ce9c0302fed798abeb
khalyomede <>
2018-05-20 00:43:15 +0200

    fixed typo

--- src/js/parse-int.js
+++ src/js/parse-int.js
@@ -5,7 +5,7 @@ if( 'parseInt' in Object.prototype === false ) {
      * @summary Parse the element and returns its integer counterpart.
      * @returns {Integer}
      * @example "42".parseInt() // 42
-     * @since [[ VERSION HERE ]]
+     * @since 0.1.0
      * @see https://cdn.rawgit.com/stellar-labs/prototype-storm/master/dist/parse-int.min.js
      * @see https://github.com/stellar-labs/prototype-storm/blob/master/src/js/parse-int.js
      * @see https://github.com/stellar-labs/prototype-storm/blob/master/test/parse-int.js
